5b0bc1b1afb6433a7ab81e83ff4ca7c6a08b623c
16soratod <sdoken@stuy.edu>
2016-03-07 05:51:06 -0500

    new lines of codes

--- 06MergeSort/Sorts.java
+++ 06MergeSort/Sorts.java
@@ -23,7 +23,60 @@ o the left.
     }
 
     public static void mergeSort(int[] data){
-        
+        mergeH(data, 0, half(data.length), half(data.length) + 1, data.length - 1);
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////
+    public static void merge(int[] data, int start, int end, int start1, int end1){
+        int[][] nData = new int[end1 - start1 + (end - start) + 2];
+        int count = start;
+        if(start == start1){
+            return;
+        }
+        if(start1<start){
+            count = start1;
+        }
+        for(int i = 0; i < nData.length; i++){
+            if(start > end){
+                nData[i] = data[start1];
+                start1++;
+            }else if(start > end){
+                nData[i] = data[start];
+                start++;
+            }else if(data[start] <= data[start1]){
+                nData[i] = data[start];
+            }else if(data[start1] < data[start]){
+                nData[i] = data[start1];
+                start1++;
+            }
+        }
+        for(int i = 0l i < nData.length; i++){
+            data[i + count] = nData[i];
+        }
+    }
+    public static int half(int a){
+        if(a % 2 != 0){
+            return (a-1)/2;
+        }else{
+            return a/2;
+        }
+    }
+    public static mergeH(int[] data, int start, int end, int start1, int end1){
+        if(start == end && start1 == end1){
+            merge(data, start, end, start1, end1);
+        }else if(start == end){
+            mergeH(data, start, end, start1, end1);
+            mergeH(data, start1, start1+half(end1-start1), start1+half(end1-start1)+1, end1);
+            merge(data, start, end, start1, end1);
+        }else if(start1 == end1){
+            mergeH(data, start, start+ half(end-start), start+half(end-start)+1, end);
+            mergeH(data, start1, end1, start1, end1);
+            merge(data,start,end,start1,end1);
+        }else{
+            mergeH(data, start1, start1 + half(end-start), start + half(end-start) + 1, end);
+            mergeH(data, start1, start1 + half(end1-start1), start1 + half(end1 - start1), end1);
+            merge(data, start, end, start1, end1);
+        }
     }
 
     public static void main(String[]args){
