2af34070cd242a2c832f910e7fc1656822499602
16soratod <sdoken@stuy.edu>
2016-03-10 10:14:04 -0500

    new lines of code

--- 07QuickSort/Quick.java
+++ 07QuickSort/Quick.java
@@ -17,7 +17,7 @@ public class Quick{
 	private static int partition(int[] data, int left, int right){
 		//random
 		int index = (int)(Math.random()*(right-left+1));
-
+		//int index = (right-left+1) / 2;
 		System.out.println(index);
 		//write a swap fxn
 		swap(data, index, left);
@@ -25,7 +25,7 @@ public class Quick{
 		int start = left + 1;
 		int end = right;
 		while(start < end){
-			if (data[start] > data[left]){
+			if (data[start] > data[left] && data[start] < data[end]){
 				swap(data, start, end);
 				end--;
 			}else{
@@ -59,16 +59,17 @@ public class Quick{
 		}
 	}
 
-///////////////////////////////////////////////////////////////////////////
+/////////////////////////~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/////////////////////////
 	
 	public static void quickSort(int[]data){
 		quickSort(data,0,data.length-1);
 	}
 
 	private static void quickSort(int[]data,int left,int right){
-		if(data.length > 0 && left <= right){
-			quickSort(data, left, partition(data, left, right)-1);
-			quickSort(data, partition(data,left,right)+1,right);
+		if(left < right){
+			int index = partition(data,left,right);
+			quickSort(data, left, index-1);
+			quickSort(data, index+1,right);
 		}
 	}
 
@@ -79,11 +80,11 @@ public class Quick{
 		System.out.println("array before: 7,8,4,5,6,9,0,2,3,1");
 		//System.out.println("array after: 0,2,3,5,7,8,9 Answer should be 5");
 		//System.out.println(a.partition(array,0,array.length-1));
-		System.out.println(a.quickselect(array, 3));
+		//System.out.println(a.quickselect(array, 3));
+		//System.out.println(Arrays.toString(array));
+		//int[] array2 = new int[]{7,8,4,5,6,9,0,2,3,1};
+		a.quickSort(array);
 		System.out.println(Arrays.toString(array));
-		int[] array2 = new int[]{7,8,4,5,6,9,0,2,3,1};
-		a.quickSort(array2);
-		System.out.println(Arrays.toString(array2));
 		
 	}
 
