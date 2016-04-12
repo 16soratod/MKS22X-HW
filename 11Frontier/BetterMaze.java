08477570a564f4e863d91924f51e9fda2a459592
16soratod <sdoken@stuy.edu>
2016-04-12 12:53:09 -0400

    code

--- 11Frontier/BetterMaze.java
+++ 11Frontier/BetterMaze.java
@@ -1,18 +1,47 @@
 public class BetterMaze{
-	pricate char[][] maze;
-	int[] solution;
-	int startRow, startCol;
-	FrontierQ<Node> placesToGo;
-	boolean animate; //default false
 
-	public int[] solutionCoordinates(){
+    private char[][] maze;
+    private int[]    solution;
+    private int      startRow,startCol;
+    private Frontier<Node> placesToGo;
+    private boolean  animate;//default to false
 
-	}
-	public boolean solveBFS(){
-		placesToGo = new FrontierQ<Node>;
-		return solve();
-	}
+   /**return a COPY of solution.
+     *This should be : [x1,y1,x2,y2,x3,y3...]
+     *the coordinates of the solution from start to end.
+     *Precondition : one of the solveXXX methods has already been 
+     * called (solveBFS OR solveDFS OR solveAStar)
+     *(otherwise an empty array is returned)
+     *Postcondition:  the correct solution is in the returned array
+    **/
+    public int[] solutionCoordinates(){
+        /** IMPLEMENT THIS **/      
+    }    
 
-	public void setAnimate(boolean b){ animate = b;}
-	private void makeSolution
+    /**initialize the frontier as a queue and call solve
+    **/
+    public boolean solveBFS(){  
+        /** IMPLEMENT THIS **/ 
+    }   
+
+   /**initialize the frontier as a stack and call solve
+    **/ 
+    public boolean solveDFS(){  
+        /** IMPLEMENT THIS **/  
+    }    
+
+   /**Search for the end of the maze using the frontier. 
+      Keep going until you find a solution or run out of 
+      elements on the frontier.
+    When animate is true, print the maze each time you 
+    process a new node.
+    **/
+    public boolean solve(){  
+        /** IMPLEMENT THIS **/  
+    }    
+     
+   /**mutator for the animate variable  **/
+    public void setAnimate(boolean b){  /** IMPLEMENT THIS **/ }   
+
+	//print out # of solution coordinates lenght / 2 = steps
 }
--- 11Frontier/PathFinding.java
+++ 11Frontier/PathFinding.java
@@ -7,16 +7,16 @@ public class PathFinding{
 	}	
 
 	public class FrontierStack<T> implements Frontier<T>{
-    /***Make This Work This Weekend!***/
-    /***You can use your classes or built in ones***/
-    /***You can extend another class OR wrap around it***/
+    	/***Make This Work This Weekend!***/
+    	/***You can use your classes or built in ones***/
+    	/***You can extend another class OR wrap around it***/
 
 	}
 
 	public class FrontierQueue<T> implements Frontier<T>{
-    /***Make This Work This Weekend!***/
-    /***You can use your classes or built in ones***/
-    /***You can extend another class OR wrap around it***/
+    	/***Make This Work This Weekend!***/
+    	/***You can use your classes or built in ones***/
+    	/***You can extend another class OR wrap around it***/
 
 	}
 }	
