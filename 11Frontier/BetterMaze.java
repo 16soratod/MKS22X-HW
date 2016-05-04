5c4e72fe093f9b95cdda4f47ac1c2688d2f84c6b
16soratod <sdoken@stuy.edu>
2016-05-04 13:15:02 -0400

    code

--- 11Frontier/BetterMaze.java
+++ 11Frontier/BetterMaze.java
@@ -1,3 +1,5 @@
+import java.io*;
+import java.util*;
 public class BetterMaze{
 
     private char[][] maze;
@@ -5,6 +7,30 @@ public class BetterMaze{
     private int      startRow,startCol;
     private Frontier<Node> placesToGo;
     private boolean  animate;//default to false
+
+    private class Node{
+        private int r,c;
+        private Node prev;
+
+        public Node(int row, int col, Node p){
+            r = row;
+            c = col;
+            prev = p;
+        }
+
+        public int getRow(){
+            return r;
+        }
+        public int getCol(){
+            return c;
+        }
+        public Node getPrevious(){
+            return prev;
+        }
+        public String toString(){
+            return "Row: " + r + " , Col: " + c;
+        }
+    }
 
    /**return a COPY of solution.
      *This should be : [x1,y1,x2,y2,x3,y3...]
@@ -16,18 +42,40 @@ public class BetterMaze{
     **/
     public int[] solutionCoordinates(){
         /** IMPLEMENT THIS **/      
+        if(solved){
+
+        }
     }    
 
     /**initialize the frontier as a queue and call solve
     **/
     public boolean solveBFS(){  
         /** IMPLEMENT THIS **/ 
+        placesToGo = new FrontierQueue<Node>();
+        Node local =  new Node(startCol,startRow,null);
+        placesToGo.add(local);
+        while(!(local.getRow() == endRow && local.getCol() == endCol)){
+            maze[current.getY()][current.getX()] = '.';
+            for(Node cur : getNeighbors(current) ){ 
+                placesToGo.add(cur);
+            }
+            if (animate) {
+                System.out.println(this.toString());  
+                wait(5);
+            }
+            try{
+                current = placesToGo.next();
+            }catch(NoSuchElementException e){
+                return false;
+            }
     }   
 
    /**initialize the frontier as a stack and call solve
     **/ 
     public boolean solveDFS(){  
         /** IMPLEMENT THIS **/  
+        placesToGo = new FrontierStack<Node>();
+        return solve();        
     }    
 
    /**Search for the end of the maze using the frontier. 
@@ -41,7 +89,24 @@ public class BetterMaze{
     }    
      
    /**mutator for the animate variable  **/
-    public void setAnimate(boolean b){  /** IMPLEMENT THIS **/ }   
+    public void setAnimate(boolean b){  animate = b; }   
 
 	//print out # of solution coordinates lenght / 2 = steps
+
+    /*while(placesToGo.hasNext()){
+        Node next = placesToGo.next();
+
+        //process neighbors
+        for(Node n : process(next)){
+            //is it the end?
+            if(checkEnd(n)){
+                makeSolution(n);
+                return true;
+            }
+        }
+    }
+    for(Node n : getNeighbors(current)){
+    
+    }
+    */
 }
--- 11Frontier/PathFinding.java
+++ 11Frontier/PathFinding.java
@@ -1,3 +1,5 @@
+import java.io*;
+import java.util*;
 public class PathFinding{
 	public interface Frontier<T>{
    		public void add(T element);
@@ -7,13 +9,46 @@ public class PathFinding{
 	}	
 
 	public class FrontierStack<T> implements Frontier<T>{
+		public Stack<T> data;
+		public FrontierStack(){
+			data = new Stack<T>();
+		}
+		public void add(T e){
+			data.push(e);
+		}
+		public T next(){
+			return data.pop();
+		}
+		public boolean hasNext(){
+			if(data.empty){
+				return false;
+			}else{
+				return true;
+			}
+		}
     	/***Make This Work This Weekend!***/
     	/***You can use your classes or built in ones***/
     	/***You can extend another class OR wrap around it***/
 
 	}
-
-	public class FrontierQueue<T> implements Frontier<T>{
+	public class FrontierQueue<T> implements Frontier<T>{	
+		public ArrayDeque<T> data;
+		public FrontierQueue(){
+			data = new ArrayDeque<T>();
+		}
+		public void add(T e){
+			data.addLast(e);
+		}
+		public T next(){
+			return data.removeFirst();
+		}
+		public boolean hasNext(){
+			if(data.isEmpty()){
+				return false;
+			}else{
+				return true;
+			}
+		}
     	/***Make This Work This Weekend!***/
     	/***You can use your classes or built in ones***/
     	/***You can extend another class OR wrap around it***/
